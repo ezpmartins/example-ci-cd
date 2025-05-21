95ec962529c66336f580b867d6f657c4054dfa5e
Enzo Martins <enzo.martins@sptech.school>
2025-05-21 10:22:45 -0300

    chore: setup inicial

---
+++ src/main/java/com/example/example_ci_cd/ExampleCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.example_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCiCdApplication.class, args);
+	}
+
+}
